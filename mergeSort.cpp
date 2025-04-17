960cb790fe61be02c9fe14d854d8116661cd07e1
ZakiAndika9 <m.zaki.andika9@gmail.com>
2025-04-17 10:30:35 +0700

    first commit

--- mergeSort.cpp
+++ mergeSort.cpp
@@ -1,1 +1,17 @@
 #include <iostream>
+#include <string>
+using namespace std;
+
+void input(){
+	while (true) {
+		cout << "Masukkan Panjang element array: ";
+		cin >> n;
+
+		if (n<= 20) {
+			break;
+		}
+		else {
+			cout << "\nMaksimal panjang array adalah 20";
+		}
+	}
+}
